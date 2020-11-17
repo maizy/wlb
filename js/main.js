199accafb1831486f7ce35d863a96b27a5ba33d1
Nikita Kovalev <nikita@maizy.ru>
2020-11-17 12:24:21 +0300

    iss #2: estimate end of the work day for the last open work sprint

--- js/main.js
+++ js/main.js
@@ -2,28 +2,32 @@ if (window.wlb === undefined) {
   (function () {
     // save input value to localstorage
 
-    const SaveInput = function () {
-      this.timeInput = null;
-      this.lsKey = "input";
+    const MemoizableInput = function (elementId, lsKey) {
+      this.elementId = elementId;
+      this.input = null;
+      this.lsKey = lsKey;
     };
 
-    SaveInput.prototype.init = function () {
-      this.timeInput = document.getElementById("time-input");
+    MemoizableInput.prototype.init = function () {
+      this.input = document.getElementById(this.elementId);
       this.ls = window.localStorage;
       if (this.ls) {
-        this.timeInput.value = this.ls.getItem(this.lsKey);
+        const lsValue = this.ls.getItem(this.lsKey)
+        if (lsValue !== null) {
+          this.input.value = this.ls.getItem(this.lsKey);
+        }
         this.bindAll();
       }
     };
 
-    SaveInput.prototype.bindAll = function () {
-      this.timeInput.addEventListener("input", this.onValueChange.bind(this));
-      this.timeInput.addEventListener("change", this.onValueChange.bind(this));
+    MemoizableInput.prototype.bindAll = function () {
+      this.input.addEventListener("input", this.onValueChange.bind(this));
+      this.input.addEventListener("change", this.onValueChange.bind(this));
     };
 
-    SaveInput.prototype.onValueChange = function () {
-      const currentValue = this.timeInput.value;
-      // console.debug("value changed", currentValue);
+    MemoizableInput.prototype.onValueChange = function () {
+      const currentValue = this.input.value;
+      //console.debug("value changed", currentValue);
       this.ls.setItem(this.lsKey, currentValue);
     };
 
@@ -60,6 +64,11 @@ if (window.wlb === undefined) {
       this.minute = Math.min(59, Math.max(0, minute || 0));
     }
 
+    Time.fromMinutesOfDay = function (minutes) {
+      minutes = Math.min(60*24, Math.max(0, minutes || 0));
+      return new Time(Math.floor(minutes / 60), minutes % 60);
+    }
+
     Time.prototype.formatted = function () {
       const minuteStr = (this.minute + '').padStart(2, '0');
       return this.hour + ":" + minuteStr;
@@ -71,35 +80,96 @@ if (window.wlb === undefined) {
 
     Time.prototype.compare = function (other) {
       if (other.minutesOfDay() < this.minutesOfDay()) {
-        return -1;
-      } else if (other.minutesOfDay() > this.minutesOfDay()) {
         return 1;
+      } else if (other.minutesOfDay() > this.minutesOfDay()) {
+        return -1;
       }
       return 0;
     };
 
+    Time.prototype.ge = function (other) {
+      return this.compare(other) >= 0;
+    };
+
+    Time.prototype.gt = function (other) {
+      return this.compare(other) > 0;
+    };
+
+    Time.prototype.le = function (other) {
+      return this.compare(other) <= 0;
+    };
+
+    Time.prototype.lt = function (other) {
+      return this.compare(other) < 0;
+    };
+
     Time.prototype.durationUntil = function (other) {
       let cmp = this.compare(other);
-      if (cmp > 0) {
+      if (cmp < 0) {
         return new Duration(other.minutesOfDay() - this.minutesOfDay());
-      } else if (cmp < 0) { // day wrap
+      } else if (cmp > 0) { // day wrap
         return new Duration(60*24 - this.minutesOfDay() + other.minutesOfDay());
       }
       return new Duration(0);
     };
 
-    function MkTime(hour, minute) {
-      return new Time(hour, minute);
-    }
+    Time.prototype.plus = function (duration) {
+      let minutesOfDay = this.minutesOfDay() + duration.minutes;
+      minutesOfDay = minutesOfDay % (60 * 24);
+      return Time.fromMinutesOfDay(minutesOfDay);
+    };
 
-    function Now() {
+    Time.prototype.minus = function (duration) {
+      let minutesOfDay = this.minutesOfDay() - duration.minutes;
+      minutesOfDay = minutesOfDay % (60 * 24);
+      if (minutesOfDay < 0) {
+        minutesOfDay = 24 * 60 + minutesOfDay;
+      }
+      return Time.fromMinutesOfDay(minutesOfDay);
+    };
+
+    Time.MkTime = function (hour, minute) {
+      return new Time(hour, minute);
+    };
+
+    Time.Now = function () {
       const now = new Date();
-      return MkTime(now.getHours(), now.getMinutes());
-    }
+      return Time.MkTime(now.getHours(), now.getMinutes());
+    };
 
 
     const Duration = function (minutes) {
       this.minutes = Math.max(0, minutes || 0);
+    }
+
+    const DurationHelpers = {
+      number: new RegExp(/\d+/),
+      onlyNumeric: new RegExp(/^\d+$/),
+      durationSpec: new RegExp(/\d+[ .:-]*[hmd]*/, "g"),
+    }
+
+    Duration.parse = function (value) {
+      value = value.toLowerCase();
+      if (value.match(DurationHelpers.onlyNumeric)) {
+        return new Duration(parseInt(value) * 60);
+      } else {
+        const matchedSpecs = value.matchAll(DurationHelpers.durationSpec);
+        let minutes = 0;
+        for (const spec of matchedSpecs) {
+          const numberMatch = spec[0].match(DurationHelpers.number);
+          if (numberMatch !== null) {
+            const number = parseInt(numberMatch[0]);
+            if (spec[0].includes("m")) {
+              minutes += number;
+            } else if (spec[0].includes("d")) {
+              minutes += 24*60*number;
+            } else {
+              minutes += number * 60;
+            }
+          }
+        }
+        return new Duration(minutes);
+      }
     }
 
     Duration.prototype.formatted = function () {
@@ -116,7 +186,36 @@ if (window.wlb === undefined) {
 
     Duration.prototype.plus = function (other) {
       return new Duration(this.minutes + other.minutes);
-    }
+    };
+
+    Duration.prototype.minus = function (other) {
+      return new Duration(this.minutes - other.minutes);
+    };
+
+    Duration.prototype.compare = function (other) {
+      if (other.minutes < this.minutes) {
+        return 1;
+      } else if (other.minutes > this.minutes) {
+        return -1;
+      }
+      return 0;
+    };
+
+    Duration.prototype.ge = function (other) {
+      return this.compare(other) >= 0;
+    };
+
+    Duration.prototype.gt = function (other) {
+      return this.compare(other) > 0;
+    };
+
+    Duration.prototype.le = function (other) {
+      return this.compare(other) <= 0;
+    };
+
+    Duration.prototype.lt = function (other) {
+      return this.compare(other) < 0;
+    };
 
     // parse today working periods
 
@@ -124,17 +223,22 @@ if (window.wlb === undefined) {
       // compile regexps for speedup matching
       timeRegExp: new RegExp(/^\s*(?<hour>[0-9]+)[^0-9]*(?<minute>[0-9]*)\s*$/),
       beginEndRegExp: new RegExp(/\s+[a-z\-–\t]*\s*/, "i"),
-      newLineRegExp: new RegExp(/\r?\n/)
+      newLineRegExp: new RegExp(/\r?\n/),
+      minimumBreakRatio: 0.5
     };
 
     const Today = function () {
       this.timeInput = null;
+      this.workTime = null;
+      this.lunchBreak = null;
       this.todayPeriods = null;
       this.updateInterval = null;
     };
 
     Today.prototype.init = function () {
       this.timeInput = document.getElementById("time-input");
+      this.workTime = document.getElementById("work-time");
+      this.lunchBreak = document.getElementById("lunch-break");
       this.todayPeriods = document.getElementById("today-periods");
 
       this.timeInput.addEventListener("input", this.updateWorkingPeriods.bind(this));
@@ -159,7 +263,7 @@ if (window.wlb === undefined) {
         if (timeMatch.groups.minute) {
           minute = parseInt(timeMatch.groups.minute) || 0
         }
-        return MkTime(parseInt(timeMatch.groups.hour), minute)
+        return Time.MkTime(parseInt(timeMatch.groups.hour), minute)
       }
       return null;
     };
@@ -173,7 +277,7 @@ if (window.wlb === undefined) {
       if (parts.length === 1) {
         begin = this._parseTime(parts[0]);
         if (begin !== null) {
-          end = Now();
+          end = Time.Now();
           assumedEnd = true;
           success = true;
         }
@@ -217,7 +321,6 @@ if (window.wlb === undefined) {
       const lenHeader = document.createElement("th");
       lenHeader.innerText = "Length";
       header.appendChild(lenHeader);
-
       table.append(header);
 
       for (const period of enriched.periods) {
@@ -232,6 +335,10 @@ if (window.wlb === undefined) {
         if (period.assumedEnd) {
           end.className = 'assumed';
         }
+        // FIXME add badge
+        if (period.estimateEnd !== undefined) {
+          end.innerText += '\nest: ' + period.estimateEnd.formatted();
+        }
         line.appendChild(end);
 
         const len = document.createElement("td");
@@ -243,7 +350,6 @@ if (window.wlb === undefined) {
 
         table.appendChild(line);
       }
-
       this.todayPeriods.appendChild(table);
 
       const summary = document.createElement("div");
@@ -254,7 +360,6 @@ if (window.wlb === undefined) {
       summary.append(totalLabel);
       summary.append(' ');
       summary.append(enriched.totalDuration.formatted());
-
       this.todayPeriods.appendChild(summary);
 
     };
@@ -278,28 +383,75 @@ if (window.wlb === undefined) {
 
     Today.prototype.enrichWorkingPeriods = function (periods) {
       const durations = [];
+      const breaks = [];
+      periods.sort(function(a, b) {
+        // day wrap
+        if (a.begin.gt(a.end) && b.begin.lt(b.end)) {
+          return -1;
+        } else if (b.begin.gt(b.end) && a.begin.lt(a.end)) {
+          return 1;
+        }
+        return a.begin.compare(b.begin) || a.end.compare(b.end);
+      });
+      let previousEnd = null;
       for (const period of periods) {
         const duration = period.begin.durationUntil(period.end);
         period.duration = duration;
         durations.push(duration);
+
+        if (previousEnd !== null) {
+          breaks.push(previousEnd.durationUntil(period.begin));
+        }
+        previousEnd = period.end;
       }
       const totalDuration = durations.reduce((acc, x) => acc.plus(x), new Duration(0));
+      const totalBreaks = breaks.reduce((acc, x) => acc.plus(x), new Duration(0));
+      const workTime = Duration.parse(this.workTime.value);
+      const lunchBreak = Duration.parse(this.lunchBreak.value);
+
+      // console.debug("totalDuration", totalDuration.formatted());
+      // console.debug("totalBreaks", totalBreaks.formatted());
+
+      let elapsed = null;
+      if (workTime.gt(totalDuration)) {
+        elapsed = workTime.minus(totalDuration);
+        if (totalBreaks.minutes === 0 || totalBreaks.minutes < lunchBreak.minutes * TodayHelpers.minimumBreakRatio) {
+          elapsed = elapsed.plus(lunchBreak);
+        }
+      }
+
+      if (elapsed !== null && periods.length > 0 && periods[periods.length - 1].assumedEnd) {
+        periods[periods.length - 1].estimateEnd = periods[periods.length - 1].end.plus(elapsed);
+      }
+      // TODO: estimate next periods
+
+      // console.debug("breaks", breaks);
+      // if (elapsed !== null) {
+      //   console.debug("elapsed", elapsed.formatted());
+      // }
+
       return {
         periods,
-        totalDuration
+        totalDuration,
+        totalBreaks,
+        elapsed
       };
     };
 
 
     window.wlb = {
       init: function () {
-        this.saveInput.init();
-        this.inputExamples.init();
-        this.today.init();
+        for (const prop in window.wlb) {
+          if (prop !== "init" && window.wlb.hasOwnProperty(prop) && window.wlb[prop].init !== undefined) {
+            window.wlb[prop].init();
+          }
+        }
       },
-      saveInput: new SaveInput(),
+      saveInput: new MemoizableInput("time-input", "input"),
+      saveWorkTimeConfig: new MemoizableInput("work-time", "work-time"),
+      saveLunchBreakConfig: new MemoizableInput("lunch-break", "lunch-break"),
       inputExamples: new InputExamples(),
-      today: new Today(),
+      today: new Today()
     };
 
     window.wlb.init();
