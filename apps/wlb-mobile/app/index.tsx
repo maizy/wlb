08606736c916f6ba0e68629303377f82aef99df6
Nikita Kovalev <nikita@maizy.dev>
2024-07-20 11:49:56 +0200

    iss #12: icon & splash screen

--- apps/wlb-mobile/app/index.tsx
+++ apps/wlb-mobile/app/index.tsx
@@ -6,7 +6,7 @@ import {enrichWorkPeriods} from "wlb-intelligence/intelligence/workday";
 import {WorkdaySettings} from "wlb-intelligence/data/workdaySettings";
 import {Duration} from "wlb-intelligence/data/duration";
 import {TodayWorkPeriods} from "@/components/TodayWorkPeriods";
-import {useCurrentTime} from "@/hooks/useCurrentTime";
+import useCurrentTime from "@/hooks/useCurrentTime";
 import useStoredState from "@/hooks/useStoredState";
 
 const styles = StyleSheet.create({
@@ -27,13 +27,23 @@ const styles = StyleSheet.create({
       },
     })
   },
+  block: {
+    marginTop: 15
+  },
+  blockTitle: {
+    fontSize: 18,
+    fontFamily: 'Merriweather_700Bold',
+  },
+  blockContent: {
+    marginTop: 10,
+  },
 });
 
 const workdaySettings = new WorkdaySettings(new Duration(8 * 60), new Duration(60));
 
 export default function Index() {
   const now = useCurrentTime();
-  const [workPeriodsSpec, setWorkPeriodsSpec] = useStoredState<string>('work-periods', '', x => x, x => x);
+  const [workPeriodsSpec, setWorkPeriodsSpec] = useStoredState<string>('work-periods', '9:00 - ', x => x, x => x);
   const onChange = (text: string) => {
     setWorkPeriodsSpec(text)
   };
@@ -49,16 +59,19 @@ export default function Index() {
   return (
     <SafeAreaView style={{padding: 10}}>
       <Text style={styles.logo}>WLB</Text>
-      <TextInput
-        style={styles.input}
-        multiline={true}
-        value={workPeriodsSpec}
-        autoComplete="off"
-        onChangeText={onChange}
-      ></TextInput>
-      <View style={{marginTop: 15}}>
-        <Text style={{fontSize: 18, fontFamily: 'Merriweather_700Bold'}}>Today</Text>
-        <TodayWorkPeriods periods={enriched.periods} style={{marginTop: 10}}/>
+      <View style={styles.block}>
+        <Text style={styles.blockTitle}>Worktime</Text>
+        <TextInput
+          style={[styles.input, styles.blockContent]}
+          multiline={true}
+          value={workPeriodsSpec}
+          autoComplete="off"
+          onChangeText={onChange}
+        ></TextInput>
+      </View>
+      <View style={styles.block}>
+        <Text style={styles.blockTitle}>Today</Text>
+        <TodayWorkPeriods periods={enriched.periods} style={styles.blockContent}/>
       </View>
     </SafeAreaView>
   );
--- apps/wlb-mobile/hooks/useCurrentTime.ts
+++ apps/wlb-mobile/hooks/useCurrentTime.ts
@@ -5,7 +5,7 @@ import {Time, makeTime} from "wlb-intelligence/data/time";
  * This hacky hook only works because React will re-execute the hook
  * after its state has eventually changed after the timeout.
  */
-export function useCurrentTime(maxUpdateIterval?: number): Time {
+export default function useCurrentTime(maxUpdateIterval?: number): Time {
   const [now, setNow] = useState<Time>(makeTime());
 
   useEffect(() => {
